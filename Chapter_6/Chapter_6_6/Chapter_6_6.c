56ced9875fca942ba14f585fa2512b16e5fba5a6
jternence <378995869@qq.com>
2016-12-06 19:08:33 +0800

    新增Chapter_6_6章节内容,使用顶点缓冲来实现画三角形

--- Chapter_6/Chapter_6_6/Chapter_6_6.c
+++ Chapter_6/Chapter_6_6/Chapter_6_6.c
@@ -5,6 +5,7 @@
 
 int init (ESContext *esContext){
     UserData *userData = esContext->userData;
+
     const char vShaderStr[] =
             "#version 300 es                            \n"
                     "layout(location = 0) in vec4 a_position;   \n"
@@ -19,30 +20,35 @@ int init (ESContext *esContext){
 
     const char fShaderStr[] =
             "#version 300 es            \n"
-             "precision mediump float;   \n"
-             "in vec4 v_color;           \n"
-             "out vec4 o_fragColor;      \n"
-             "void main()                \n"
-              "{                          \n"
-              "    o_fragColor = v_color; \n"
-              "}" ;
+                    "precision mediump float;   \n"
+                    "in vec4 v_color;           \n"
+                    "out vec4 o_fragColor;      \n"
+                    "void main()                \n"
+                    "{                          \n"
+                    "    o_fragColor = v_color; \n"
+                    "}" ;
 
+    //项目程序句柄
     GLuint programObject;
-    // Create the program object
-    programObject = esLoadProgram (vShaderStr, fShaderStr );
 
-    if ( programObject == 0 ) {
+    //创建程序对象
+    programObject = esLoadProgram(vShaderStr,fShaderStr);
+
+    if(programObject == 0){
         return GL_FALSE;
     }
 
-    // Store the program object
+    //保存程序对象
     userData->programObject = programObject;
 
-    //清除面板颜色
-    glClearColor ( 1.0f, 1.0f, 1.0f, 0.0f );
+    userData->vboIds[0]     = 0;
+    userData->vboIds[1]     = 0;
+    userData->vboIds[2]     = 0;
+
+    //将屏幕颜色设置为白色
+    glClearColor(1.0f,1.0f,1.0f,1.0f);
 
     return GL_TRUE;
-
 }
 
 /**
@@ -51,25 +57,85 @@ int init (ESContext *esContext){
  */
 void Draw ( ESContext *esContext){
     UserData *userData = esContext->userData;
-    GLfloat color[4] = { 0.0f, 0.0f, 1.0f, 0.0f };
-    // 3 vertices, with (x,y,z) per-vertex
-    GLfloat vertexPos[3 * 3] =
+    // 3 vertices, with (x,y,z) ,(r, g, b, a) per-vertex
+    GLfloat vertexPos[3 * VERTEX_POS_SIZE] =
             {
-                    0.0f,  0.5f, 0.0f, // v0
-                    -0.5f, -0.5f, 0.0f, // v1
-                    0.5f, -0.5f, 0.0f  // v2
+                    0.0f,  0.5f, 0.0f,        // v0
+                    -0.5f, -0.5f, 0.0f,        // v1
+                    0.5f, -0.5f, 0.0f         // v2
             };
-    //设置平截头体
+    GLfloat color[4 * VERTEX_COLOR_SIZE] =
+            {
+                    1.0f, 0.0f, 0.0f, 1.0f,   // c0
+                    0.0f, 1.0f, 0.0f, 1.0f,   // c1
+                    0.0f, 0.0f, 1.0f, 1.0f    // c2
+            };
+
+    GLint vtxStrides[2] =
+            {
+                    VERTEX_POS_SIZE * sizeof ( GLfloat ),
+                    VERTEX_COLOR_SIZE * sizeof ( GLfloat )
+            };
+
+    // Index buffer data
+    GLushort indices[3] = { 0, 1, 2 };
+    GLfloat *vtxBuf[2] = { vertexPos, color };
+
     glViewport ( 0, 0, esContext->width, esContext->height );
-    //清除颜色缓冲区
     glClear ( GL_COLOR_BUFFER_BIT );
-    //使用程序项目
     glUseProgram ( userData->programObject );
-    glVertexAttribPointer ( 0, 3, GL_FLOAT, GL_FALSE, 0, vertexPos );
-    glEnableVertexAttribArray ( 0 );
-    glVertexAttrib4fv ( 1, color );
-    glDrawArrays ( GL_TRIANGLES, 0, 3 );
-    glDisableVertexAttribArray (1);
+    drawPrimitiveWithVBOs ( esContext, 3, vtxBuf,
+                            vtxStrides, 3, indices );
+}
+
+void drawPrimitiveWithVBOs ( ESContext *esContext,
+                             GLint numVertices, GLfloat **vtxBuf,
+                             GLint *vtxStrides, GLint numIndices,
+                             GLushort *indices ) {
+    UserData *userData = esContext->userData;
+    // vboIds[0] - used to store vertex position
+    // vboIds[1] - used to store vertex color
+    // vboIds[2] - used to store element indices
+    if ( userData->vboIds[0] == 0 && userData->vboIds[1] == 0 &&
+         userData->vboIds[2] == 0 ) {
+        // Only allocate on the first draw
+        glGenBuffers ( 3, userData->vboIds );
+
+        glBindBuffer ( GL_ARRAY_BUFFER, userData->vboIds[0] );
+        glBufferData ( GL_ARRAY_BUFFER, vtxStrides[0] * numVertices,
+                       vtxBuf[0], GL_STATIC_DRAW );
+
+        glBindBuffer ( GL_ARRAY_BUFFER, userData->vboIds[1] );
+        glBufferData ( GL_ARRAY_BUFFER, vtxStrides[1] * numVertices,
+                       vtxBuf[1], GL_STATIC_DRAW );
+        glBindBuffer ( GL_ELEMENT_ARRAY_BUFFER, userData->vboIds[2] );
+        glBufferData ( GL_ELEMENT_ARRAY_BUFFER,
+                       sizeof ( GLushort ) * numIndices,
+                       indices, GL_STATIC_DRAW );
+    }
+
+
+    glBindBuffer ( GL_ARRAY_BUFFER, userData->vboIds[0] );
+    glEnableVertexAttribArray ( VERTEX_POS_INDX );
+    glVertexAttribPointer ( VERTEX_POS_INDX, VERTEX_POS_SIZE,
+                            GL_FLOAT, GL_FALSE, vtxStrides[0], 0 );
+
+    glBindBuffer ( GL_ARRAY_BUFFER, userData->vboIds[1] );
+    glEnableVertexAttribArray ( VERTEX_COLOR_INDX );
+    glVertexAttribPointer ( VERTEX_COLOR_INDX,
+                            VERTEX_COLOR_SIZE,
+                            GL_FLOAT, GL_FALSE, vtxStrides[1], 0 );
+
+    glBindBuffer ( GL_ELEMENT_ARRAY_BUFFER, userData->vboIds[2] );
+
+    glDrawElements ( GL_TRIANGLES, numIndices,
+                     GL_UNSIGNED_SHORT, 0 );
+
+    glDisableVertexAttribArray ( VERTEX_POS_INDX );
+    glDisableVertexAttribArray ( VERTEX_COLOR_INDX );
+
+    glBindBuffer ( GL_ARRAY_BUFFER, 0 );
+    glBindBuffer ( GL_ELEMENT_ARRAY_BUFFER, 0 );
 }
 
 /**
@@ -78,8 +144,20 @@ void Draw ( ESContext *esContext){
  */
 void Shutdown ( ESContext *esContext){
     UserData *userData = esContext->userData;
-    glDeleteProgram (userData->programObject );
+    glDeleteProgram ( userData->programObject );
+    glDeleteBuffers ( 3, userData->vboIds );
 }
 
+int esMain ( ESContext *esContext ) {
+    esContext->userData = malloc (sizeof ( UserData ) );
+    esCreateWindow ( esContext, "Example 6-6", 320, 240, ES_WINDOW_RGB );
+
+    if ( !init ( esContext ) ) {
+        return GL_FALSE;
+    }
+    esRegisterShutdownFunc ( esContext, Shutdown );
+    esRegisterDrawFunc ( esContext, Draw );
+    return GL_TRUE;
+}
 
 
--- Chapter_6/Chapter_6_6/include/Chapter_6_6.h
+++ Chapter_6/Chapter_6_6/include/Chapter_6_6.h
@@ -6,9 +6,47 @@
 #define OPENGLES_CHAPTER_6_6_H
 
 #include "esUtil.h"
+
+#define VERTEX_POS_SIZE       3 // x, y and z
+#define VERTEX_COLOR_SIZE     4 // r, g, b, and a
+
+#define VERTEX_POS_INDX       0
+#define VERTEX_COLOR_INDX     1
+
 typedef struct{
     GLuint programObject;
     // VertexBufferObject Ids
     GLuint vboIds[3];
-} ;
+} UserData;
+
+/**
+ * draw something
+ * @param esContext
+ */
+void Draw ( ESContext *esContext );
+/**
+ * 窗口关闭回调函数
+ * @param esContext
+ */
+void Shutdown ( ESContext *esContext );
+
+/**
+ * 通过vbo
+ * @param esContext
+ * @param numVertices
+ * @param vtxBuf
+ * @param vtxStrides
+ * @param numIndices
+ * @param indices
+ */
+void drawPrimitiveWithVBOs ( ESContext *esContext,
+                             GLint numVertices, GLfloat **vtxBuf,
+                             GLint *vtxStrides, GLint numIndices,
+                             GLushort *indices );
+
+/**
+ * 初始化配置
+ * @param esContext
+ */
+int init(ESContext *esContext);
 #endif //OPENGLES_CHAPTER_6_6_H
